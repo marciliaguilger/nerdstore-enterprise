03b49a5c570fb37ddfe9d4f4d8fcfa06bd42f92e
Marcilia Da Silva Guilger <marcilia.guilger@ifood.com.br>
2022-08-24 07:49:56 -0300

    add identity packages

---
+++ src/services/NSE.Identity.API/Configuration/DependencyInjectionConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NSE.Identity.API.Data;
+
+namespace NSE.Identity.API.Configuration
+{
+    public static class DependencyInjectionConfiguration
+    {
+        public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+        }
+    }
+}
+
--- src/services/NSE.Identity.API/Program.cs
+++ src/services/NSE.Identity.API/Program.cs
@@ -1,15 +1,16 @@
+using NSE.Identity.API.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.ConfigureDbContext(builder.Configuration);
+builder.Services.ConfigureIdentity();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,6 +19,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthorization();
 app.UseAuthorization();
 
 app.MapControllers();
