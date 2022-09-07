52bc746c13fd423234c99ee21da914e8f15c4ea7
Marcilia Da Silva Guilger <marcilia.guilger@ifood.com.br>
2022-09-07 18:26:13 -0300

    configure identity

---
+++ src/web/NSE.WebApp.MVC/Configuration/WebAppConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace NSE.WebApp.MVC.Configuration
+{
+    public static class WebAppConfiguration
+    {
+        public static void AddMvcConfiguration(this IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public static void UseApiConfiguration(this WebApplication app)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseIdentityConfiguration();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        }
+    }
+}
