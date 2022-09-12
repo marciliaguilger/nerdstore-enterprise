270ca39620eeb42e22c752dda181176a2c38b7c3
Marcilia Da Silva Guilger <marcilia.guilger@ifood.com.br>
2022-09-12 07:15:11 -0300

    add login view

--- src/web/NSE.WebApp.MVC/Controllers/IdentityController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/IdentityController.cs
@@ -1,8 +1,58 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentityController : Controller
     {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return View(usuarioRegistro);
+
+            //api de registro
+
+            if (false) return View(usuarioRegistro);
+
+            //realizar login
+            return RedirectToAction("Index", "Home");
+        }
+        
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            //api de login
+
+            if (false) return View(usuarioLogin);
+
+            //realizar login
+            return RedirectToAction("Index", "Home");
+        }
+        
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
