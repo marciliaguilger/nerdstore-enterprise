77e2cbc3a6a38bd0cb82787702a6fee9a20d4f02
Marcilia Da Silva Guilger <marcilia.guilger@ifood.com.br>
2023-02-10 10:39:45 -0300

    costumer modeling

---
+++ src/services/NSE.Customer.API/Models/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using NSE.Core.DomainObjects;
+
+namespace NSE.Customer.API.Models
+{
+    public class Cliente : Entity, IAggregateRoot
+    {
+
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+        public bool Excluido { get; private set; }
+        public Endereco Endereco { get; private set; }
+        public Guid ClienteId { get; private set; }
+
+
+        public Cliente(string nome, string email, string cpf, bool excluido, Endereco endereco)
+        {
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+            Excluido = false;
+        }
+        //EF Core
+        protected Cliente() {}
+    }
+}
---
+++ src/services/NSE.Customer.API/Models/Endereco.cs
@@ -0,0 +1,26 @@
+﻿using NSE.Core.DomainObjects;
+
+namespace NSE.Customer.API.Models
+{
+    public class Endereco : Entity
+    {
+        public string Logradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Bairro { get; private set; }
+        public string Cep { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
+        
+        // EF Relation
+        public Cliente Cliente { get; protected set; }
+        public Endereco(string logradouro, string numero, string bairro, string cep, string cidade, string estado)
+        {
+            Logradouro = logradouro;
+            Numero = numero;
+            Bairro = bairro;
+            Cep = cep;
+            Cidade = cidade;
+            Estado = estado;
+        }
+    }
+}
